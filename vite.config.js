fe70c035a9b3e8d6b9dcf2f26ec3bd397b91febf
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-12-03 23:51:44 -0800

    fix another issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0', 
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: 'all' 
+  }
 })
