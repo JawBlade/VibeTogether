4cc6e18111c6090c5be6bdb683ad0dbb80ab65f9
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-12-04 10:23:35 -0800

    Added Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
+import { HiHome } from "react-icons/hi";
+import { GoSearch } from "react-icons/go";
+
 function Navbar(){ 
   return (
-    <>
-        <img src='public/logo2.png' className='w-32 h-32'></img>
-        <div className='text-white text-5xl outline'>Hello</div>
+    <>  
+        <div className='flex items-center outline text-white m-1'>
+            <div>
+                <img src="/logo2.png" className="content-center w-32 h-32 rounded-full object-cover object-center" />
+            </div>
+
+            <div className="outline content-center rounded-full object-cover object-center m-4">
+                <HiHome />
+            </div>
+
+            <div className="outline content-center rounded-full object-cover object-center">
+                <GoSearch />
+            </div>
+        </div>
     </>
   );
 };
