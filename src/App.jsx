7be4513d54c5ca2d2405686d7bfbc77180fcb45c
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-12-04 09:28:12 -0800

    Made a New Logo

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div className="outline text-5xl bg-[#121212] text-white">Hello</div>
-  )
-}
-
-export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){ 
+  return (
+    <>
+        <img src='public/logo2.png'></img>
+        <div className='text-white text-5xl outline'>Hello</div>
+    </>
+  );
+};
+
+export default Navbar;
