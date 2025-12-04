9d0c0a5ea8fe3ca1a0365d7dceeffbbebfbeb30c
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-12-04 09:34:24 -0800

    Resized Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar(){ 
   return (
     <>
-        <img src='public/logo2.png'></img>
+        <img src='public/logo2.png' className='w-32 h-32'></img>
         <div className='text-white text-5xl outline'>Hello</div>
     </>
   );
