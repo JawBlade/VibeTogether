6f79fc1f9451259e4f180e148b0ab11ed2b04c3b
JawBlade <122082998+JawBlade@users.noreply.github.com>
2025-12-04 22:43:42 -0800

    Made The Home Button and Search Bar Animated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,23 +7,32 @@ function Navbar(){
     <>  
         <div className='flex justify-between items-center outline text-white m-1 p-2'>
             <div>
-                <img src="/logo2.png" className="content-center w-15 h-15 rounded-full object-cover object-center" />
+                <img src="/logo2.png" className="content-center w-16 h-16 rounded-full object-cover object-center" />
             </div>
 
-            <div className="flex items-center space-x-4 content-center rounded-full object-cover object-center m-4">
+            <div className="flex items-center space-x-2 m-4">
 
-                <div className="outline rounded-full p-2 ">
-                    <HiHome />
-                </div>
-
-                <div className="w-50 outline rounded-full p-2">
-                    <GoSearch />
-                </div>
-                
+            {/* Home Icon */}
+            <div className="rounded-full p-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] transition duration-200 ease-in transform hover:scale-105">
+                <HiHome className="h-7 w-7 " />
             </div>
 
-            <div className="outline content-center rounded-full object-cover object-center">
-                <CgProfile />
+            {/* Search Bar */}
+            <div className="flex items-center rounded-full p-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] transition-colors duration-200 ease-in hover:border border-white/10">
+
+                <GoSearch className="h-7 w-7 mr-2 text-gray-300 group-hover:text-white" />
+
+                <input
+                type="text"
+                placeholder="What do you want to play?"
+                className="bg-transparent text-white group-hover:text-white focus:outline-none w-64"
+                />
+            </div>
+
+            </div>
+
+            <div className="outline content-center rounded-full object-cover object-center mr-4">
+                <CgProfile className="h-10 w-10" />
             </div>
         </div>
     </>
